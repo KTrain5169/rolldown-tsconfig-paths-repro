8ef1d87447330c75f63d4909980f263818a43601
KTrain5369 <me@git.ktrain5369.dev>
2026-03-06 13:13:11 +1000

    add jsx fx in app/
    separating this from previous commit to preserve commit history

--- app/index.tsx
+++ app/index.tsx
@@ -1,3 +1,11 @@
 import { function1 } from "1/index";
 
 function1()
+
+export function JSXFunction() {
+    return <>
+        <div class="test">
+            <p>Hello world</p>
+        </div>
+    </>
+}
